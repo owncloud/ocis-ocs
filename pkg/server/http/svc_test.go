7e961a95c7bfb5193b34c246d0cfd58752ef0fc6
Dipak Acharya <dpakach@gmail.com>
2020-08-31 10:25:34 +0545

    Refactor tests to include both ocs api versions

--- pkg/server/http/svc_test.go
+++ pkg/server/http/svc_test.go
@@ -31,6 +31,7 @@ import (
 
 var service = grpc.Service{}
 
+var ocsVersions = []string{"v1.php", "v2.php"}
 var formats = []string{"json", "xml"}
 
 var DefaultUsers = []string{
@@ -86,12 +87,20 @@ type Meta struct {
 	Message    string `json:"message" xml:"message"`
 }
 
-func (m *Meta) Success() bool {
-	if m.Status == "ok" &&
-		(m.StatusCode >= 100 && m.StatusCode <= 300) {
-		return true
-	}
-	return false
+func (m *Meta) Success(ocsVersion string) bool {
+  if !(ocsVersion == "v1.php" || ocsVersion == "v2.php") {
+    return false
+  }
+  if m.Status != "ok" {
+    return false
+  }
+	if (ocsVersion == "v1.php" && m.StatusCode != 100) {
+		return false
+	} else if (ocsVersion == "v2.php" && m.StatusCode != 200) {
+    return false
+  } else {
+	  return true
+  }
 }
 
 type SingleUserResponse struct {
@@ -313,42 +322,68 @@ func TestCreateUser(t *testing.T) {
 		},
 	}
 
-	for _, format := range formats {
-		for _, data := range testData {
-			formatpart := getFormatString(format)
-			res, err := sendRequest(
-				"POST",
-				"/v1.php/cloud/users"+formatpart,
-				data.user.getUserRequestString(),
-				"admin:admin",
-			)
+  for _, ocsVersion := range ocsVersions {
+    for _, format := range formats {
+      for _, data := range testData {
+        formatpart := getFormatString(format)
+        res, err := sendRequest(
+          "POST",
+          fmt.Sprintf("/%v/cloud/users%v", ocsVersion, formatpart),
+          data.user.getUserRequestString(),
+          "admin:admin",
+        )
 
-			if err != nil {
-				t.Fatal(err)
-			}
+        if err != nil {
+          t.Fatal(err)
+        }
 
-			var response SingleUserResponse
+        var response SingleUserResponse
 
-			if format == "json" {
-				if err := json.Unmarshal(res.Body.Bytes(), &response); err != nil {
-					t.Fatal(err)
-				}
-			} else {
-				if err := xml.Unmarshal(res.Body.Bytes(), &response); err != nil {
-					t.Fatal(err)
-				}
-			}
+        if format == "json" {
+          if err := json.Unmarshal(res.Body.Bytes(), &response); err != nil {
+            t.Fatal(err)
+          }
+        } else {
+          if err := xml.Unmarshal(res.Body.Bytes(), &response); err != nil {
+            t.Fatal(err)
+          }
+        }
 
-			if data.err == nil {
-				assert.True(t, response.Meta.Success(), "The response was expected to be successful but was not")
-				// assertUserSame(t, data.user, response.Data)
-			} else {
-				assertResponseMeta(t, *data.err, response.Meta)
-			}
-		}
-	}
+        if data.err == nil {
+          assert.True(t, response.Meta.Success(ocsVersion), "The response was expected to be successful but was not")
+          // assertUserSame(t, data.user, response.Data)
+        } else {
+          assertResponseMeta(t, *data.err, response.Meta)
+        }
 
-	cleanUp(t)
+        res, err = sendRequest(
+          "GET",
+          "/v1.php/cloud/users?format=json",
+          "",
+          "admin:admin",
+        )
+
+        if err != nil {
+          t.Fatal(err)
+        }
+
+        var usersResponse GetUsersResponse
+
+        if err := json.Unmarshal(res.Body.Bytes(), &usersResponse); err != nil {
+          t.Fatal(err)
+        }
+
+        assert.True(t, usersResponse.Meta.Success("v1.php"), "The response was expected to be successful but was not")
+
+        if data.err == nil {
+          assert.Contains(t, usersResponse.Data.Users, data.user.ID)
+        } else {
+          assert.NotContains(t, usersResponse.Data.Users, data.user.Username)
+        }
+        cleanUp(t)
+      }
+    }
+  }
 }
 
 func TestGetUsers(t *testing.T) {
@@ -369,117 +404,125 @@ func TestGetUsers(t *testing.T) {
 		},
 	}
 
-	for _, format := range formats {
-		for _, user := range users {
-			err := createUser(user)
-			if err != nil {
-				t.Fatal(err)
-			}
-		}
+  for _, ocsVersion := range ocsVersions {
+    for _, format := range formats {
+      for _, user := range users {
+        err := createUser(user)
+        if err != nil {
+          t.Fatal(err)
+        }
+      }
 
-		formatpart := getFormatString(format)
-		res, err := sendRequest(
-			"GET",
-			"/v1.php/cloud/users"+formatpart,
-			"",
-			"admin:admin",
-		)
+      formatpart := getFormatString(format)
+      res, err := sendRequest(
+        "GET",
+        fmt.Sprintf("/%v/cloud/users%v", ocsVersion, formatpart),
+        "",
+        "admin:admin",
+      )
 
-		if err != nil {
-			t.Fatal(err)
-		}
+      if err != nil {
+        t.Fatal(err)
+      }
 
-		var response GetUsersResponse
+      var response GetUsersResponse
 
-		if format == "json" {
-			if err := json.Unmarshal(res.Body.Bytes(), &response); err != nil {
-				t.Fatal(err)
-			}
-		} else {
-			if err := xml.Unmarshal(res.Body.Bytes(), &response); err != nil {
-				t.Fatal(err)
-			}
-		}
+      if format == "json" {
+        if err := json.Unmarshal(res.Body.Bytes(), &response); err != nil {
+          t.Fatal(err)
+        }
+      } else {
+        if err := xml.Unmarshal(res.Body.Bytes(), &response); err != nil {
+          t.Fatal(err)
+        }
+      }
 
-		assert.True(t, response.Meta.Success(), "The response was expected to be successful but was not")
-		for _, user := range users {
-			assert.Contains(t, response.Data.Users, user.Username)
-		}
-		cleanUp(t)
-	}
+      assert.True(t, response.Meta.Success(ocsVersion), "The response was expected to be successful but was not")
+      for _, user := range users {
+        assert.Contains(t, response.Data.Users, user.Username)
+      }
+      cleanUp(t)
+    }
+  }
 }
 
 func TestGetUsersDefaultUsers(t *testing.T) {
-	for _, format := range formats {
-		formatpart := getFormatString(format)
-		res, err := sendRequest(
-			"GET",
-			"/v1.php/cloud/users"+formatpart,
-			"",
-			"admin:admin",
-		)
+  for _, ocsVersion := range ocsVersions {
+    for _, format := range formats {
+      formatpart := getFormatString(format)
+      res, err := sendRequest(
+        "GET",
+        fmt.Sprintf("/%v/cloud/users%v", ocsVersion, formatpart),
+        "",
+        "admin:admin",
+      )
 
-		if err != nil {
-			t.Fatal(err)
-		}
+      if err != nil {
+        t.Fatal(err)
+      }
 
-		var response GetUsersResponse
-		if format == "json" {
-			if err := json.Unmarshal(res.Body.Bytes(), &response); err != nil {
-				t.Fatal(err)
-			}
-		} else {
-			if err := xml.Unmarshal(res.Body.Bytes(), &response); err != nil {
-				t.Fatal(err)
-			}
-		}
+      var response GetUsersResponse
 
-		assert.True(t, response.Meta.Success(), "The response was expected to be successful but was not")
-		for _, user := range DefaultUsers {
-			assert.Contains(t, response.Data.Users, user)
-		}
-		cleanUp(t)
-	}
+      if format == "json" {
+        if err := json.Unmarshal(res.Body.Bytes(), &response); err != nil {
+          t.Fatal(err)
+        }
+      } else {
+        if err := xml.Unmarshal(res.Body.Bytes(), &response); err != nil {
+          t.Fatal(err)
+        }
+      }
+
+      assert.True(t, response.Meta.Success(ocsVersion), "The response was expected to be successful but was not")
+      assert.Empty(t, response.Data.Users)
+      // for _, user := range DefaultUsers {
+      //   assert.Contains(t, response.Data.Users, user)
+      // }
+      cleanUp(t)
+    }
+  }
 }
 
 func TestGetUserDefaultUser(t *testing.T) {
-	for _, format := range formats {
-		formatpart := getFormatString(format)
-		for _, user := range DefaultUsers {
-			res, err := sendRequest(
-				"GET",
-				fmt.Sprintf("/v1.php/cloud/user/%s", user)+formatpart,
-				"",
-				"admin:admin",
-			)
+  for _, ocsVersion := range ocsVersions {
+    for _, format := range formats {
+      formatpart := getFormatString(format)
+      for _, user := range DefaultUsers {
+        res, err := sendRequest(
+          "GET",
+          fmt.Sprintf("/%s/cloud/user/%s%s", ocsVersion, user, formatpart),
+          "",
+          "admin:admin",
+        )
 
-			if err != nil {
-				t.Fatal(err)
-			}
+        if err != nil {
+          t.Fatal(err)
+        }
 
-			var response SingleUserResponse
-			if format == "json" {
-				if err := json.Unmarshal(res.Body.Bytes(), &response); err != nil {
-					t.Fatal(err)
-				}
-			} else {
-				if err := xml.Unmarshal(res.Body.Bytes(), &response); err != nil {
-					t.Fatal(err)
-				}
-			}
+        var response SingleUserResponse
+        if format == "json" {
+          if err := json.Unmarshal(res.Body.Bytes(), &response); err != nil {
+            t.Fatal(err)
+          }
+        } else {
+          if err := xml.Unmarshal(res.Body.Bytes(), &response); err != nil {
+            t.Fatal(err)
+          }
+        }
 
-			assertResponseMeta(t, Meta{
-				Status:     "error",
-				StatusCode: 998,
-				Message:    "not found",
-			}, response.Meta)
-			cleanUp(t)
-		}
-	}
+        assertResponseMeta(t, Meta{
+          Status:     "error",
+          StatusCode: 998,
+          Message:    "not found",
+        }, response.Meta)
+        cleanUp(t)
+      }
+    }
+  }
 }
 
 func TestDeleteUser(t *testing.T) {
-	time.Sleep(time.Second * 2)
+	time.Sleep(time.Second * 1)
 	users := []User{
 		{
 			Enabled:     "true",
@@ -497,63 +540,65 @@ func TestDeleteUser(t *testing.T) {
 		},
 	}
 
-	for _, format := range formats {
-		for _, user := range users {
-			err := createUser(user)
-			if err != nil {
-				t.Fatal(err)
-			}
-		}
+  for _, ocsVersion := range ocsVersions {
+    for _, format := range formats {
+      for _, user := range users {
+        err := createUser(user)
+        if err != nil {
+          t.Fatal(err)
+        }
+      }
 
-		formatpart := getFormatString(format)
-		res, err := sendRequest(
-			"DELETE",
-			"/v1.php/cloud/users/rutherford"+formatpart,
-			"",
-			"admin:admin",
-		)
+      formatpart := getFormatString(format)
+      res, err := sendRequest(
+        "DELETE",
+        fmt.Sprintf("/%s/cloud/users/rutherford%s", ocsVersion, formatpart),
+        "",
+        "admin:admin",
+      )
 
-		if err != nil {
-			t.Fatal(err)
-		}
+      if err != nil {
+        t.Fatal(err)
+      }
 
-		var response DeleteUserRespone
-		if format == "json" {
-			if err := json.Unmarshal(res.Body.Bytes(), &response); err != nil {
-				t.Fatal(err)
-			}
-		} else {
-			if err := xml.Unmarshal(res.Body.Bytes(), &response); err != nil {
-				t.Fatal(err)
-			}
-		}
+      var response DeleteUserRespone
+      if format == "json" {
+        if err := json.Unmarshal(res.Body.Bytes(), &response); err != nil {
+          t.Fatal(err)
+        }
+      } else {
+        if err := xml.Unmarshal(res.Body.Bytes(), &response); err != nil {
+          t.Fatal(err)
+        }
+      }
 
-		assert.True(t, response.Meta.Success(), "The response was expected to be successful but was not")
-		assert.Empty(t, response.Data)
+      assert.True(t, response.Meta.Success(ocsVersion), "The response was expected to be successful but was not")
+      assert.Empty(t, response.Data)
 
-		// Check deleted user doesn't exist and the other user does
-		res, err = sendRequest(
-			"GET",
-			"/v1.php/cloud/users?format=json",
-			"",
-			"admin:admin",
-		)
+      // Check deleted user doesn't exist and the other user does
+      res, err = sendRequest(
+        "GET",
+        "/v1.php/cloud/users?format=json",
+        "",
+        "admin:admin",
+      )
 
-		if err != nil {
-			t.Fatal(err)
-		}
+      if err != nil {
+        t.Fatal(err)
+      }
 
-		var usersResponse GetUsersResponse
-		if err := json.Unmarshal(res.Body.Bytes(), &usersResponse); err != nil {
-			t.Fatal(err)
-		}
+      var usersResponse GetUsersResponse
+      if err := json.Unmarshal(res.Body.Bytes(), &usersResponse); err != nil {
+        t.Fatal(err)
+      }
 
-		assert.True(t, usersResponse.Meta.Success(), "The response was expected to be successful but was not")
-		assert.Contains(t, usersResponse.Data.Users, "thomson")
-		assert.NotContains(t, usersResponse.Data.Users, "rutherford")
+      assert.True(t, usersResponse.Meta.Success("v1.php"), "The response was expected to be successful but was not")
+      assert.Contains(t, usersResponse.Data.Users, "thomson")
+      assert.NotContains(t, usersResponse.Data.Users, "rutherford")
 
-		cleanUp(t)
-	}
+      cleanUp(t)
+    }
+  }
 }
 
 func TestUpdateUser(t *testing.T) {
@@ -575,46 +620,48 @@ func TestUpdateUser(t *testing.T) {
 		},
 	}
 
-	for _, format := range formats {
-		formatpart := getFormatString(format)
-		for _, user := range users {
-			err := createUser(user)
-			if err != nil {
-				t.Fatal(err)
-			}
-		}
+  for _, ocsVersion := range ocsVersions {
+    for _, format := range formats {
+      formatpart := getFormatString(format)
+      for _, user := range users {
+        err := createUser(user)
+        if err != nil {
+          t.Fatal(err)
+        }
+      }
 
-		res, err := sendRequest(
-			"PUT",
-			"/v1.php/cloud/users/rutherford"+formatpart,
-			"username=chadwick&displayname=James%20Chadwick",
-			"admin:admin",
-		)
+      res, err := sendRequest(
+        "PUT",
+        fmt.Sprintf("/%s/cloud/users/rutherford%s", ocsVersion, formatpart),
+        "username=chadwick&displayname=James%20Chadwick",
+        "admin:admin",
+      )
 
-		if err != nil {
-			t.Fatal(err)
-		}
+      if err != nil {
+        t.Fatal(err)
+      }
 
-		var response struct {
-			Meta Meta `json:"meta" xml:"meta"`
-		}
+      var response struct {
+        Meta Meta `json:"meta" xml:"meta"`
+      }
 
-		if format == "json" {
-			if err := json.Unmarshal(res.Body.Bytes(), &response); err != nil {
-				t.Fatal(err)
-			}
-		} else {
-			if err := xml.Unmarshal(res.Body.Bytes(), &response); err != nil {
-				t.Fatal(err)
-			}
-		}
+      if format == "json" {
+        if err := json.Unmarshal(res.Body.Bytes(), &response); err != nil {
+          t.Fatal(err)
+        }
+      } else {
+        if err := xml.Unmarshal(res.Body.Bytes(), &response); err != nil {
+          t.Fatal(err)
+        }
+      }
 
-		assertResponseMeta(t, Meta{
-			Status:     "error",
-			StatusCode: 103,
-			Message:    "unknown key ''",
-		}, response.Meta)
+      assertResponseMeta(t, Meta{
+        Status:     "error",
+        StatusCode: 103,
+        Message:    "unknown key ''",
+      }, response.Meta)
 
-		cleanUp(t)
-	}
+      cleanUp(t)
+    }
+  }
 }
